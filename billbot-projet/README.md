6702c15b04e2404f72f453488d547278b1f39bd3
Sphinx06 <xavier.grimaldi@live.fr>
2017-05-10 11:06:23 +0200

    delete

--- billbot-projet/README.md
+++
@@ -1,4 +0,0 @@
-billbot-projet
-==============
-
-A Symfony project created on May 9, 2017, 8:49 am.
--- billbot-projet/src/BillbotBundle/BillbotBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BillbotBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BillbotBundle extends Bundle
-{
-}
